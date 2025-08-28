7fa8416e09548970e293128c705587e16bf5c109
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-08-27 20:13:36 -0400

    avance 4-average

--- practica-js/js/4-average.js
+++ practica-js/js/4-average.js
@@ -2,8 +2,10 @@ let count = 0
 let sumaNotas = 0;
 while(count < 4) {
     let nota = parseInt(prompt("Ingrese un valor entero para la nota"));
+    console.log(`La nota ingresada es: ${nota}`);
     sumaNotas = sumaNotas + nota;
     count++;
 }
-
+console.log(`La suma de las notas es: ${sumaNotas}`);
 let promedio = sumaNotas / count;
+console.log(`El promedio de notas es: ${promedio}`);
