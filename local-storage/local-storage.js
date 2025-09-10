69ddb57cba0c8dbd6fe3dfe0e74a41db1b319301
Carlos F. Martínez Sánchez <carlos.f.martinez.s+github@gmail.com>
2025-09-10 19:50:01 -0300

    uso de localStorage

--- local-storage/local-storage.js
+++ local-storage/local-storage.js
@@ -1,6 +1,6 @@
 function guardar() {
-    const street = document.getElementById('calle').value
-    const num = document.getElementById('numero').value
+    const calle = document.getElementById('calle').value
+    const numero = document.getElementById('numero').value
 
     let addresses = localStorage.getItem("direcciones")
     if (!addresses) {
@@ -9,10 +9,58 @@ function guardar() {
         addresses = JSON.parse(addresses)
     }
 
-    addresses.push({
-        street: street,
-        num: num
+    let address = {
+        street: calle,
+        num: numero
+    }
+
+    addresses.push(address);
+
+    localStorage.setItem("direcciones", JSON.stringify(addresses))
+
+    document.getElementById('calle').value = ''
+    document.getElementById('numero').value = ''
+
+    mostrarMensaje(`Dirección ${calle} ${numero} guardada correctamente` )
+}
+
+function mostrar() {
+    let addresses = localStorage.getItem("direcciones")
+    if (!addresses) {
+        addresses = []
+    } else {
+        addresses = JSON.parse(addresses)
+    }
+
+    const div = document.getElementById('datos')
+    div.innerHTML = ''
+
+    addresses.forEach(address => {
+        const p = document.createElement('p')
+        p.innerText = `${address.street} ${address.num}`
+        div.appendChild(p)
     });
 
-    localStorage.setItem("direcciones", JSON.stringify(datos))
+    /*for (const address of addresses) {
+        const p = document.createElement('p')
+        p.innerText = `${address.street} ${address.num}`
+        div.appendChild(p)
+    }
+
+    for (let index = 0; index < addresses.length; index++) {
+        const element = addresses[index];
+        const p = document.createElement('p')
+        p.innerText = `${element.street} ${element.num}`
+        div.appendChild(p)
+    }*/
+}
+
+function ocultarMensaje() {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = '';
+}
+
+function mostrarMensaje(msg) {
+    const messageDiv = document.getElementById('message');
+    messageDiv.innerText = msg;
 }
